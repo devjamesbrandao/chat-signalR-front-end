c9089da64c5c39575010e47d6a48d3b281d752ef
James Noleto <muricijames@gmail.com>
2022-08-27 21:42:51 -0300

    feat: componentes da aplicação

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import Lobby from './components/Lobby';
+import Chat from './components/Chat';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  const [connection, setConnection] = useState();
+  const [messages, setMessages] = useState([]);
+  const [currentUser, setCurrentUser] = useState();
+  const [users, setUsers] = useState([]);
+
+  const joinRoom = async (user, room) => {
+    try {
+      const connection = new HubConnectionBuilder()
+        .withUrl(`${process.env.REACT_APP_BASE_URL}/chat`)
+        .configureLogging(LogLevel.Information)
+        .build();
+
+      connection.on("ReceiveMessage", (user, message, id) => {
+        setMessages(messages => [...messages, { user, message, id }]);
+      });
+
+      connection.on("UsersInRoom", (users) => {
+        setUsers(users);
+      });
+
+      connection.onclose(e => {
+        setConnection();
+        setMessages([]);
+        setUsers([]);
+      });
+
+      await connection.start();
+
+      var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var passwordLength = 12;
+      var id = "";
+
+      for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        id += chars.substring(randomNumber, randomNumber +1);
+       }
+
+
+      await connection.invoke("JoinRoom", { user, room, id });
+
+      setCurrentUser(id);
+
+        setConnection(connection);
+    } catch (e) {
+      console.log("Erro: " + e);
+      alert("Erro: " + e);
+    }
+  }
+
+  const sendMessage = async (message) => {
+    try {
+      await connection.invoke("SendMessage", message);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const closeConnection = async () => {
+    try {
+      await connection.stop();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return <div className='app'>
+    <h2>DEV Chat</h2>
+    <hr className='line' />
+    {!connection
+      ? <Lobby joinRoom={joinRoom} />
+      : <Chat sendMessage={sendMessage} 
+              messages={messages} 
+              users={users} 
+              currentUser={currentUser}
+              closeConnection={closeConnection} 
+        />}
+  </div>
+}
+
+export default App;
